3d3e8e6a0a76ee09a14720da30bc8109d86bca29
Lisa Scott <lisarenesoctt@gmail.com>
2019-04-24 22:04:55 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+ @@all = []
+
+ def initialize(name)
+   @name = name
+   @@all << self
+end
+
+  def self.clear_all
+    @@all = []
+
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name 
+    end
+  end
+
+  # def self.new(name)
+  #   @@all << name
+  # end
+
+
+
+
+end
