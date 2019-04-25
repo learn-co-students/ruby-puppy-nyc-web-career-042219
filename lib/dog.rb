3d7174e2c627ed1861da34ee8af15cf0e66c7493
Kane Koo <kanekoo@gmail.com>
2019-04-25 07:40:54 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Dog
+
+  @@all = []
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @@all << self
+    # binding.pry
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each {|dog| puts "#{dog.name}"}
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
